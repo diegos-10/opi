ff526bea2de0190f0817e633b0f5e7a9da217d4b
opinbergroup <opinbergroup@gmail.com>
2023-07-02 15:20:23 -0500

    build.sh test

--- opinbercrud/settings.py
+++ opinbercrud/settings.py
@@ -80,14 +80,21 @@ WSGI_APPLICATION = 'opinbercrud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 
 DATABASES = {
-    'default':  dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres', conn_max_age=600
-    )    
-}
+     'default':  dj_database_url.config(
+         default='postgresql://postgres:postgres@localhost/postgres', conn_max_age=600
+     )
+ }
 
- 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
